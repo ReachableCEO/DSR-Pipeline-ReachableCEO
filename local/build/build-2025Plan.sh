78dcc5de06b0d8d6f0617530a79259990d5626ad
Charles N Wyble <reachableceo@turnsys.com>
2024-12-16 09:19:07 -0600

    .

---
+++ local/build/build-2025Plan.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# This is a demo script for the DSR-Pipeline-Server
+# This script creates PDF output from markdown input
+
+
+###############@##################################
+#Edit the below file to reflect your information
+##################################################
+
+source "../DSRVariables.env"
+
+####################################################
+####################################################
+####################################################
+#DO NOT CHANGE ANYTHING BELOW THIS LINE
+####################################################
+####################################################
+####################################################
+
+############################################################################################
+# Setup key variables that will be used by the create-stakeholder-output-server.sh script
+############################################################################################
+
+export MO_PATH="bash ../../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
+
+echo "Cleaning up from previous runs..."
+
+rm $BUILDYAML_STAKEHOLDER_OUTPUT || true
+rm $StakeholderOutputMarkdownOutputFile || true
+rm $StakeholderOutputPDFOutputFile || true
+
+echo "Combining markdown files into single input file for pandoc..."
+cat $StakeholderOutputMarkdownInputFile > $StakeholderOutputMarkdownOutputFile
+
+#Call the build stakeholder output microservice
+echo "Calling the build stakeholder output microservice..."
+bash ../../vendor/git.knownelement.com/reachableceo/DSR-Pipeline-Server/build/build-stakeholder-output-server.sh
